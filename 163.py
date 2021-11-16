74eec8f872b6b0712a9b8a383ab47f389c1caed8
Voituk009 <90752215+Voituk009@users.noreply.github.com>
2021-11-16 10:39:34 +0200

    labaratorna16

---
+++ 163.py
@@ -0,0 +1,17 @@
+#cd D:python/python/16
+#163.py
+#Напишите программу, выполняющую различные вычисления и  
+#выводящую  значений с применение методов форматирования, 
+#манипуляторов и флагов объекта cout.
+
+x = int(input("x: "))
+y = int(input("y: "))
+
+z = x * y
+
+product = ('Добуток: {}'.format(z))
+
+print(product)
+print(product.lower())
+print(product.upper())
+print(product.title())
---
+++ 165.py
@@ -0,0 +1,18 @@
+#cd D:python/python/16
+#165.py
+#5.	Напишите программу, предлагающую пользователю ввести его фамилию, 
+#имя и отчество , а затем выводящую эти сведения в текстовый файл.
+
+name = input("Введите имя: ")
+surname = input("Введите фамилию: ")
+patronymic =input("Введите очество: ")
+
+l = [name, surname, patronymic]
+
+File = open('file.txt', 'w')
+
+for index in l:
+
+	File.write(index + '\n')
+
+File.close()
